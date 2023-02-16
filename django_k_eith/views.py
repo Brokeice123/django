e0d3e75c5b4700adc2875fce91f3fc9096d0110a
eMobilis <you@example.com>
2023-02-16 12:50:51 +0300

    Templates

---
+++ django_k_eith/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+
+def contact(request):
+    return render(request, 'Contact.html')
+
+
+def about(request):
+    return render(request, 'about.html')
+
+
+def home(request):
+    return render(request, 'home.html')
